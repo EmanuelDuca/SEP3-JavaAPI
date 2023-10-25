4fa79d98fc1418a169a3b00a034128d1b38b366b
DanTurcanIuliu <turcan.dan.ro@gmail.com>
2023-10-25 16:02:24 +0200

    Create Project

---
+++ src/main/java/com/example/sep3javaserverspring/Sep3JavaServerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sep3javaserverspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep3JavaServerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sep3JavaServerSpringApplication.class, args);
+    }
+
+}
